2270be65ca043861b618739fccd06f5663413f44
Tarun_Rathore <tarunrathore2904@gmail.com>
2023-06-20 19:46:38 +0530

    AJAX server by tarun;

---
+++ Ajax/ajax_server_JAVAS.js
@@ -0,0 +1,48 @@
+var exp=require('express');
+var app=exp();
+
+var mysql=require('mysql2');
+var con=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"dac"
+})
+
+con.connect(function(err){
+    if(!err)
+    console.log("db connect")
+    else
+    console.log("db not connect")
+})
+
+
+app.get('/home',function(req,res){
+    res.send("<h1>welcome home page</h1>")
+})
+
+app.get('/empform',function(req,res){
+    res.sendFile(__dirname+"/empformJAVAS.html")
+})
+
+app.get('/getname',function(req,res){
+    var eid=req.query.empid;
+    console.log(eid);
+
+    con.query('select * from student where roll='+eid,function(err,result){
+        if(!err)
+        {
+            str="";
+            str+="<h1>";
+            console.log("in if"+result.length);
+            str+="welcome"+result[0].name
+            str+="</h1>";
+            console.log(str);
+            res.send(str);
+        }
+    })
+})
+
+app.listen(9000,function(req,res){
+    console.log("server started at 9000")  
+})
